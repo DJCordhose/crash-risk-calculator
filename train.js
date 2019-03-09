6a0555ec78da3efc64f9d560bb639276cc216e68
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2019-03-09 14:23:50 -0800

    replaced d3 with tf.data

--- train.js
+++ train.js
@@ -1,30 +1,25 @@
 // import * as tf from '@tensorflow/tfjs';
 // import * as tfvis from '@tensorflow/tfjs-vis';
 
+const DATA_URL =
+  "https://raw.githubusercontent.com/DJCordhose/deep-learning-crash-course-notebooks/master/data/insurance-customers-1500.csv";
+
 (async () => {
-  const parser = d3.dsvFormat(';');
+  // step 1: load, plot and pre-process data
 
-  // step 1: load and pre-process data
-  const r = await fetch(
-    'https://raw.githubusercontent.com/DJCordhose/deep-learning-crash-course-notebooks/master/data/insurance-customers-1500.csv'
-  );
-  const csv = await r.text();
-  const data = await parser.parse(csv);
-
-  const inputData = data.map((entry) => {
-    return [
-      Number(entry.age),
-      Number(entry.speed),
-      Number(entry.miles)
-    ];
-  });
-  // does this look good?
-  console.log(inputData[0]);
-
-  const groups = data.map((entry) => {
-    return Number(entry.group);
+  // https://js.tensorflow.org/api/latest/#data.csv
+  const csvDataset = await tf.data.csv(DATA_URL, {
+    delimiter: ";",
+    columnConfigs: {
+      group: { isLabel: true }
+    }
   });
 
+  // https://js.tensorflow.org/api/latest/#class:data.CSVDataset
+  const columnNames = await csvDataset.columnNames();
+  console.log(columnNames);
+
+  // https://js.tensorflow.org/api/latest/#class:data.Dataset
   // 0 - red: many accidents
   // 1 - green: few or no accidents
   // 2 - yellow: in the middle
@@ -34,62 +29,107 @@
   // https://storage.googleapis.com/tfjs-vis/mnist/dist/index.html
   // https://github.com/tensorflow/tfjs-vis#renderscatterplotdata--container-surfacehtmlelement-opts---promise
 
-  const valuesAgeSpeedRed = data.filter(entry => Number(entry.group) === red).map(entry => ({x: entry.age, y: entry.speed}));
-  const valuesAgeSpeedGreen = data.filter(entry => Number(entry.group) === green).map(entry => ({x: entry.age, y: entry.speed}));
-  const valuesAgeSpeedYellow = data.filter(entry => Number(entry.group) === yellow).map(entry => ({x: entry.age, y: entry.speed}));
-  const scatterContainer = document.getElementById('scatter-surface');
-  tfvis.render.scatterplot({
-    values: [valuesAgeSpeedRed, valuesAgeSpeedGreen, valuesAgeSpeedYellow],
-    series: ['many accidents', 'few or no accidents', 'in the middle']
-  }, scatterContainer, {
-xLabel: 'Age',
-yLabel: 'Speed',
-fontSize: 24,
-zoomToFit: true,
-height: 500
-  });
+  const valuesAgeSpeedRed = await csvDataset
+    .filter(({ xs, ys }) => Number(ys.group) === red)
+    .map(({ xs, ys }) => ({ x: xs.age, y: xs.speed }))
+    .toArray();
+  const valuesAgeSpeedGreen = await csvDataset
+    .filter(({ xs, ys }) => Number(ys.group) === green)
+    .map(({ xs, ys }) => ({ x: xs.age, y: xs.speed }))
+    .toArray();
+  const valuesAgeSpeedYellow = await csvDataset
+    .filter(({ xs, ys }) => Number(ys.group) === yellow)
+    .map(({ xs, ys }) => ({ x: xs.age, y: xs.speed }))
+    .toArray();
+  const scatterContainer = document.getElementById("scatter-surface");
+  tfvis.render.scatterplot(
+    {
+      values: [valuesAgeSpeedRed, valuesAgeSpeedGreen, valuesAgeSpeedYellow],
+      series: ["many accidents", "few or no accidents", "in the middle"]
+    },
+    scatterContainer,
+    {
+      xLabel: "Age",
+      yLabel: "Speed",
+      fontSize: 24,
+      zoomToFit: true,
+      height: 500
+    }
+  );
 
   // step 2: define a simple sequential model
   const model = tf.sequential();
-  model.add(tf.layers.dense({ name: 'hidden1', units: 100, activation: 'relu', inputShape: [3] }));
-  model.add(tf.layers.dense({ name: 'hidden2', units: 100, activation: 'relu' }));
-  model.add(tf.layers.dense({ name: 'softmax', units: 3, activation: 'softmax' }));
+  model.add(
+    tf.layers.dense({
+      name: "hidden1",
+      units: 100,
+      activation: "relu",
+      inputShape: [3]
+    })
+  );
+  model.add(
+    tf.layers.dense({ name: "hidden2", units: 100, activation: "relu" })
+  );
+  model.add(
+    tf.layers.dense({ name: "softmax", units: 3, activation: "softmax" })
+  );
   model.summary();
 
-   // https://github.com/tensorflow/tfjs-vis#showmodelsummarycontainer-drawable-model-tfmodel--promise
-   const modelContainer = document.getElementById('model-surface');
-   tfvis.show.modelSummary(modelContainer, model);
+  // https://github.com/tensorflow/tfjs-vis#showmodelsummarycontainer-drawable-model-tfmodel--promise
+  const modelContainer = document.getElementById("model-surface");
+  tfvis.show.modelSummary(modelContainer, model);
 
   model.compile({
-    loss: 'categoricalCrossentropy',
-    optimizer: 'adam',
-    metrics: ['accuracy']
+    loss: "sparseCategoricalCrossentropy",
+    optimizer: "adam",
+    metrics: ["accuracy"]
   });
 
   // step 3: train the model using our data
-  const X = tf.tensor2d(inputData, [inputData.length, 3]);
-  const y = tf.oneHot(tf.tensor1d(groups, 'int32'), 3);
+  const BATCH_SIZE = 500; // was 500, down to 100 to try on mobile
+  const EPOCHS = 50; // was 500, down to 50 to make this train quickly
+  const DATA_SIZE = 1500;
 
-  const surfaceContainer = document.getElementById('metrics-surface');
-  const metrics = ['loss', 'val_loss', 'acc', 'val_acc'];
+  // this is broken, prefetch should deliver all values, but only delivers up tp 1000
+  const xs = await csvDataset
+  .prefetch(DATA_SIZE)
+  .map(({ xs, _ }) => Object.values(xs))
+  .toArray();
+
+  const ys = await csvDataset
+  .prefetch(DATA_SIZE)
+  .map(({ _, ys }) => Object.values(ys)[0])
+  .toArray();
+
+  const X = tf.tensor2d(xs, [xs.length, 3]);
+  // const y = tf.oneHot(tf.tensor1d(ys, 'int32'), 3);
+  // one hot not needed when using sparse categorical crossentropy as loss
+  const y = tf.tensor1d(ys, 'int32')
+
+  const surfaceContainer = document.getElementById("metrics-surface");
+  const metrics = ["loss", "val_loss", "acc", "val_acc"];
   const vizCallbacks = tfvis.show.fitCallbacks(surfaceContainer, metrics);
   const consoleCallbacks = {
     onEpochEnd: (...args) => {
       console.log(...args);
     },
-    onEpochBegin: (iteration) => {
+    onEpochBegin: iteration => {
       // console.clear();
       console.log(iteration);
     }
   };
 
+  // https://js.tensorflow.org/api/latest/#tf.LayersModel.fitDataset
+  // https://js.tensorflow.org/api/latest/#tf.LayersModel.fit
   const history = await model.fit(X, y, {
-    epochs: 50, // was 500, down to 50 to make this train quickly
+    epochs: EPOCHS,
     validationSplit: 0.2,
-    batchSize: 500, // was 500, down to 100 to try on mobile
+    batchSize: BATCH_SIZE,
     callbacks: vizCallbacks
     // callbacks: consoleCallbacks
   });
+
+  return;
 
   // tfvis.show.history(surfaceContainer, history, ['loss', 'val_loss', 'acc', 'val_acc'],
   // {
@@ -99,26 +139,28 @@ height: 500
 
   // step 4: evaluate the model
 
-  const {acc, loss, val_acc, val_loss} = history.history;
-  const summary = `accuracy: ${acc[acc.length -1]}, accuracy on unknown data: ${val_acc[val_acc.length -1]}`;
+  const { acc, loss, val_acc, val_loss } = history.history;
+  const summary = `accuracy: ${
+    acc[acc.length - 1]
+  }, accuracy on unknown data: ${val_acc[val_acc.length - 1]}`;
   console.log(summary);
-  const summaryContainer = document.getElementById('summary');
+  const summaryContainer = document.getElementById("summary");
   summaryContainer.textContent = summary;
   model.predict(tf.tensor([[48, 100, 10]])).print();
 
   // 0 - red: many accidents
   // 1 - green: few or no accidents
   // 2 - yellow: in the middle
-  const classNames = ['many accidents', 'few or no accidents', 'in the middle']; 
+  const classNames = ["many accidents", "few or no accidents", "in the middle"];
   const labels = tf.tensor(groups);
   const preds = model.predict(X).argMax([-1]);
 
   const confusionMatrix = await tfvis.metrics.confusionMatrix(labels, preds);
-  const matrixContainer = document.getElementById('matrix-surface');
+  const matrixContainer = document.getElementById("matrix-surface");
   tfvis.show.confusionMatrix(matrixContainer, confusionMatrix, classNames);
 
   const classAccuracy = await tfvis.metrics.perClassAccuracy(labels, preds);
-  const accuracyContainer = document.getElementById('accuracy-surface');
+  const accuracyContainer = document.getElementById("accuracy-surface");
   tfvis.show.perClassAccuracy(accuracyContainer, classAccuracy, classNames);
 
   // step 5: safe to local browser storage
@@ -126,6 +168,6 @@ height: 500
 
   // localstorage will not work with a large model (even 500/500 layer size will be too much in chrome)
   // model.save('localstorage://insurance');
-  model.save('indexeddb://insurance');
+  model.save("indexeddb://insurance");
   console.log(await tf.io.listModels());
 })();
